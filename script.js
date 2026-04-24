e84bb993d686411492df08aa2a7ef224f4156309
Oce123 <89967255+Oce123@users.noreply.github.com>
2026-04-24 23:29:13 +0200

    Add files via upload
    Menu burger

--- script.js
+++ script.js
@@ -166,3 +166,20 @@ onSnapshot(collection(db, "inscriptions"), (snapshot) => {
     });
 });
 
+// =========================
+// MENU BURGER
+// =========================
+const burger = document.getElementById("burger");
+const nav = document.getElementById("nav");
+
+if (burger && nav) {
+    burger.addEventListener("click", () => {
+        nav.classList.toggle("active");
+    });
+}
+
+document.querySelectorAll("#nav a").forEach(link => {
+    link.addEventListener("click", () => {
+        nav.classList.remove("active");
+    });
+});
